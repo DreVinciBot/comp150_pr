b2e084f2f3d61eb3b5bea9b62e3c3ce816b797a1
drevincibot <dre.vivero@gmail.com>
2019-03-09 14:11:38 -0500

    updated hw2

--- src/particle_filter_andrecleaver_copy.py
+++ src/particle_filter_andrecleaver_copy.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+# Andre Cleaver
+# COM150 PR
+# 03/07/19
+# HW2
+
 import rospy
 import numpy as np
 import os, sys
@@ -7,16 +13,15 @@ from PIL import Image
 import random
 import math
 import time
-from skimage.measure import compare_ssim as ssim
-from sklearn import preprocessing
-
+# from skimage.measure import compare_ssim as ssim
+# from sklearn import preprocessing
 
 class particle_filter:
     def __init__(self):
         # some fixed parameters
         self.RED = [0,0,255]
         self.BLUE = [255,0,0]
-        self.GREEN = [0,255,0]
+        self.ORANGE = [0,165,250]
         self.BLACK = [0,0,0]
         self.counter = 0
         self.crop_size = 100
@@ -47,6 +52,7 @@ class particle_filter:
         self.particle_array = []
         self.new_particles = []
 
+        # load pictures by providing the path location
         img1 = cv2.imread('/home/drevinci/dre_catkin_ws/src/demo/BayMap.png',1)
         img2 = cv2.imread('/home/drevinci/dre_catkin_ws/src/demo/CityMap.png',1)
         img3 = cv2.imread('/home/drevinci/dre_catkin_ws/src/demo/MarioMap.png',1)
@@ -55,12 +61,12 @@ class particle_filter:
         px1 = img1.shape
         px2 = img2.shape
         px3 = img3.shape
-
         self.imgs = [img1, img2, img3]
         self.pxs = [px1, px2, px3]
 
+        # notify if pictures are loaded.
         if not img1.any() or not img2.any() or not img3.any():
-            print("images is empty")
+            print("Images are empty")
         else:
             print("Images loaded.")
 
@@ -76,15 +82,12 @@ class particle_filter:
         y = np.random.randint(low = (-full_px[1]//2)+ self.crop_size//2 ,high = (full_px[1]//2) - self.crop_size//2)
         x = np.random.randint(low = (-full_px[0]//2)+ self.crop_size//2 ,high = (full_px[0]//2) - self.crop_size//2)
 
-        first_step = True
-
         # display coordinate of drone in traditional format
         nx = -1*int(round(yc-(yc+y)))
         ny = int(round(xc-(xc+x)))
         print("Starting position: (" + str(nx) + "," + str(ny)+")")
 
-        # generate the particles within a small circlular Distrubuition or a large image distribution
-        # radius of the circle
+        # Generate the particles within a small circlular distrubuition
         #self.part_dis = False
         if self.part_dis:
             rc = 100
@@ -102,16 +105,17 @@ class particle_filter:
             print("Number of particles: " + str(len(self.particle_array)))
 
         else:
+            # Generate the particles in a large distribution
             for i in range(0,self.particles):
-
                 xpt = np.random.randint(low = self.crop_size//2, high = full_px[0]- self.crop_size//2)
                 ypt = np.random.randint(low = self.crop_size//2, high = full_px[1] - self.crop_size//2)
-
                 self.particle_array.append([xpt,ypt, self.weight[i]])
             print("Number of particles: " + str(len(self.particle_array)))
 
         return (int(round(xc+x)), int(round(yc+y)))
 
+    # function to crop image once given a image and x and y coordinates
+    # the size of the image is set by the variable self.crop_size = 100
     def crop_image(self,f_img,x0,y0):
         cp = self.crop_size//2
         crop = f_img.copy()
@@ -123,44 +127,34 @@ class particle_filter:
         col = pixel[1]
         ch = pixel[2]
 
-        # adding noise to the cropped image
+        # adding guassian noise to the cropped image if self.noise_flag is set to True
         if self.noise_flag:
             low = -20
             high = 20
             guass = np.random.randint(low,high, size = (row,col, ch))
             noise = np.uint8(guass)
             noise = guass.reshape(row,col,ch)
-
             final_crop = crop_img + noise
             final_crop = np.clip(final_crop, 0, 255)
             crop_img = np.uint8(final_crop)
 
         return crop_img
 
+    # function to display the location of the drone and the observation location
     def region_of_interest(self,wb_img,ref_x,ref_y,i):
         self.counter = self.counter + 1
         cp = self.crop_size//2
         img = wb_img.copy()
 
-        #img = cv2.circle(img,(drone_y,drone_x),50, self.BLUE, self.bw)
-        img = cv2.circle(img, (ref_y,ref_x),10, self.BLACK, self.bw )
-
+        # for visual of the drone in black
+        img = cv2.circle(img, (ref_y,ref_x),10, self.BLACK, 10 )
         # Red square represents the observation image by the drone
         img = cv2.rectangle(img,(ref_y-cp,ref_x-cp),(ref_y+cp,ref_x+cp),self.RED,self.bw)
 
-        # display all initial particles
-        self.part_flag = False
-        if self.part_flag:
-            self.part_flag = False
-            for t in range(len(self.particle_array)):
-                xf = self.particle_array[t][0]
-                yf = self.particle_array[t][1]
-                wt = self.particle_array[t][2]
-                img = cv2.circle(img, (yf,xf), int(wt), self.GREEN, 2)
-
-    #    print("init ", self.particle_array)
         return img
 
+    # function to randomly move the drone a distance of 1 unit = 50 pixels;
+    # this can be anagolous to the drones speed, self.speed = 50
     def movement_vector(self, x, y, i):
         x0 = x
         y0 = y
@@ -170,9 +164,12 @@ class particle_filter:
         theta = np.random.rand()*360
         dx = np.cos(np.deg2rad(theta))
         dy = np.sin(np.deg2rad(theta))
+        # add noise to the dx and dy components with mean 0 and 5 variance
         self.dx = int(dx*self.speed + np.random.normal(0,self.sensor_var))
         self.dy = int(dy*self.speed + np.random.normal(0,self.sensor_var))
 
+        # constraint to ensure drone moves within the image and allows to take a full
+        # obsercation image of crop_size
         while (x0+self.dx)-cp <= 0 or (x0+self.dx)+cp >= full_px[0] or (y0+self.dy)-cp <= 0 or (y0+self.dy)+cp >= full_px[1]:
             theta = np.random.rand()*360
             dx = np.cos(np.deg2rad(theta))
@@ -185,24 +182,21 @@ class particle_filter:
         noise_x = (x0+self.dx)
         noise_y = (y0+self.dy)
 
-        #x0+dx, y0+dy are coordinates for the reference images
         #noise_x, noise_y are coordinates for the actual drone position
         return int(noise_x), int(noise_y)
 
+    # core of the code, takes particles and calculates the error of the particles
+    # image from the observation image. I am using Root Mean Square Error (RMSE) method
     def particle_calculation(self, img, ref_image, i):
-        counter = 0
         score = 0
-        normalize = 0
         cp = self.crop_size//2
-        v = []
-        ref_row, ref_col, ref_ch = ref_image.shape
         w, h, ch = img.shape
         full_px = self.pxs[i]
 
         # center of image (xc,yc) used when shifting to new coordinate system. center of image is now (0,0)
         yc = self.pxs[i][1]*0.5
         xc = self.pxs[i][0]*0.5
-        #print("movement ", self.particle_array)
+
         # calculate the Root mean square error for all particles
         for h in range(len(self.particle_array)):
             p_image = self.imgs[i].copy()
@@ -215,8 +209,6 @@ class particle_filter:
             row, col, ch = temp_img.shape
 
             if row == self.crop_size and col == self.crop_size :
-                counter += 1
-
                 err = np.sqrt(np.mean(np.square(np.subtract(temp_img, ref_image))))
                 score = math.e ** -(err)
                 self.particle_array[h][2] = score
@@ -224,95 +216,39 @@ class particle_filter:
             else:
                 self.particle_array[h][2] = 0
 
-    #    print("good particles ", self.new_particles)
-
-        # normalize the weights
+        # normalize the weights by summing the weight column and dividing each particle weight by the total weight
         self.particle_array = np.array(self.particle_array)
         v_sum = np.sum(self.particle_array[:,2])
         for h in range(len(self.particle_array)):
                 pt_x, pt_y, pt_wt = self.particle_array[h][0], self.particle_array[h][1], self.particle_array[h][2]
                 self.particle_array[h][2] =  pt_wt/ v_sum
+                full_img = cv2.circle(img, (int(pt_y),int(pt_x)), int(self.particle_array[h][2]*100), self.ORANGE, 3)
 
-                #pt_y, pt_x = -pt_y + full_px[1]//2, pt_x + full_px[0]//2
-
-                full_img = cv2.circle(img, (int(pt_y),int(pt_x)), int(1), self.GREEN, 2)
-
-        # gather all good particles if the probability is greater than zero
+        # gather all 'good' particles if the probability is greater than zero
         nice_particles = []
         for h in range(0,len(self.particle_array)):
             if self.particle_array[h][2] > 0:
                 nice_particles.append(self.particle_array[h])
 
-    #    print("normalize ", self.new_particles)
-
         # method to resample the particles based on weighted importance with replacement
         # create array of indices from the new particles
-        #print(np.sum(a[:,2]))
         nice_particles = np.array(nice_particles)
         idx = np.random.choice(len(nice_particles), size=self.particles, p = nice_particles[:,2])
 
         for h in range(self.particles):
             self.particle_array[h] = nice_particles[idx[h]]
 
-
-
-            #full_img = cv2.circle(img, (yf,xf), int(1), self.GREEN, 2)
-
-
-        # for h in range(0, len(self.particle_array)):
-        #     # move particles and add noise for each particle
-        #     xf = self.particle_array[h][0]
-        #     yf = self.particle_array[h][1]
-        #
-        #     nx = np.random.randint(5)
-        #     ny = np.random.randint(5)
-        #     self.particle_array[h][0] = self.particle_array[h][0] + self.dx
-        #     self.particle_array[h][1] = yf + self.dy
-        #     print(xf, self.dx, self.particle_array[h][0])
-        #     self.particle_array[h][2] = 1
-            #print("dx", self.dx, "dy", self.dy)
-        # reset the weights to one
-
-
         particle_prime = []
         for h in range(len(self.particle_array)):
 
             xnoise_pt = np.random.randint(5)
             ynoise_pt = np.random.randint(5)
-            #print(self.particle_array[h][0], self.dx)
             x_new = self.particle_array[h][0] + self.dx + xnoise_pt
-            #print(self.particle_array[h][0])
-
-            #print("y: ")
-            #print(self.particle_array[h][1], self.dy)
             y_new = self.particle_array[h][1] + self.dy + ynoise_pt
-            #print(self.particle_array[h][1])
-
             particle_prime.append([x_new, y_new, 1])
             self.particle_array[h][2] = 1
 
-            #print(particle_prime, x_new, y_new)
-            #print("weight reset ", self.particle_array)
-
         self.particle_array = particle_prime
-
-        # for h in range(len(self.particle_array)):
-        #     print("x: ")
-        #     print(self.particle_array[h][0], self.dx)
-        #     self.particle_array[h][0] = self.particle_array[h][0] + self.dx
-        #     print(self.particle_array[h][0])
-        #
-        #     print("y: ")
-        #     print(self.particle_array[h][1], self.dy)
-        #     self.particle_array[h][1] = self.particle_array[h][1] + self.dy
-        #     print(self.particle_array[h][1])
-        #
-        #     self.particle_array[h][2] = 1
-        #
-        #     print("weight reset ", self.particle_array)
-
-
-
 
         return full_img
 
@@ -321,31 +257,14 @@ def main():
     initial_timestep = True
     pf = particle_filter()
 
-    i = 0
-    x0, y0 = pf.rand_location(i)
     # Working with image 0,1, or 2
+    i = 0
+
+    # function to call a random point in the coordinate plane of image.
+    # this point will serve as the drone's starting position (x0,y0)
+    x0, y0 = pf.rand_location(i)
 
     for j in range(1,100):
-
-        # function to call a random point in the coordinate plane of image.
-        # this point will serve as the drone's starting position (x0,y0)
-
-        #
-        # # function to crop image given the image, crop size, and center point x0 and y0
-        # obs_img = pf.crop_image(pf.imgs[i], x0, y0)
-        #
-        # # function to display full image with cropped region boxed
-        # full_img = pf.region_of_interest(pf.imgs[i],x0,y0,i)
-        #
-        # # display starting image
-        # height, width = full_img.shape[:2]
-        # cv2.namedWindow('jpg', cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow('jpg', width, height)
-        # cv2.imshow('jpg', full_img)
-        # cv2.waitKey(0)
-        # #cv2.destroyAllWindows()
-
-
 
         # function to move the "drone" & particles' true positino over the map
         # for each time step
@@ -360,12 +279,12 @@ def main():
         # Particle Filter Implementation
         test = pf.particle_calculation(full_img, obs_img, i)
 
-        height, width = test.shape[:2]
-        cv2.namedWindow('TimeStep ' + str(counter), cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('TimeStep ' + str(counter), width, height)
-        cv2.imshow('TimeStep ' + str(counter), test)
+        # display figure with particles and drone
+        winname = 'TimeStep ' + str(counter)
+        cv2.namedWindow(winname)        # Create a named window
+        cv2.moveWindow(winname, 40,30)  # Move it to (40,30)
+        cv2.imshow(winname, test)
         cv2.waitKey(0)
-        #cv2.destroyAllWindows()
         #cv2.destroyAllWindows()
         x0 = drone_x
         y0 = drone_y
